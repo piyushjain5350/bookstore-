ce23dec246e44256d04f30e20280be2579022b0e
Piyush Jain <piyushjain5350@gmail.com>
2023-08-26 23:51:46 +0530

    git commit -m  '1 bookstore web'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React ,{useState}from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Display from './Components/Display';
 
 function App() {
+  document.title='Bookstore';
+  const [fetchedData,setFetchedData]=useState('');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header w-full h-[10vh]">
+      <Navbar setFetchedData={setFetchedData}/>
       </header>
+      <main className="App-main  bg-slate-900	 w-full h-[100%] text-white">
+        <Display fetchedData={fetchedData} />
+      </main> 
     </div>
   );
 }
